f3bd99ba8716b1a9fad9373e7ed018fed4df7368
jamesmaino <jamesmaino@users.noreply.github.com>
2022-11-20 14:45:21 +1100

    Update run_plumber.R

--- run_plumber.R
+++ run_plumber.R
@@ -1,5 +1,4 @@
-
 library(plumber)
 # 'plumber.R' is the location of the file shown above
-pr("plumber.R") %>%
-  pr_run(port=8000)
+root <- pr(paste0(getwd(), "/plumber.R"))
+pr_run(root, host = "0.0.0.0", port = 8000)
